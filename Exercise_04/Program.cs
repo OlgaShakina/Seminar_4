afef11baeea8bbf1c2d20f2234addd8a3606dfda
Olga <olga.buinova@gmail.com>
2022-07-16 00:57:46 +0300

    Новые упражнения

---
+++ Exercise_04/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+//3, 5 -> 243 (3*3*3*3*3)
+//2, 4 -> 16
+Console.WriteLine("Введите целое число");
+int a=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("В какую степень возвезти? Введите целое число");
+int b=Convert.ToInt32(Console.ReadLine());
+
+for(int i=0;i<=b;i++){
+    
+}
