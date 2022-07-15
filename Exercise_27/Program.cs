c4df33f3f18fe44cb755155b4042dfd2ec68ddb9
Olga <olga.buinova@gmail.com>
2022-07-16 00:58:37 +0300

    новые упражнения

---
+++ Exercise_27/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//452 -< 11
+//82 -< 10
+//9012 -< 12
+Console.WriteLine("Введите целое число ");
+int a=Convert.ToInt32(Console.ReadLine());
+int sum= Convert.ToString(a)[0];
+for(int i=1;a%(Math.Pow(10,i))==a; i++){
+sum=sum+Convert.ToString(a)[i];
+}
+Console.WriteLine(sum);
---
+++ Exercise_29/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+//1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+//6, 1,33 -> [6, 1, 33]
+Console.WriteLine("Введите размер массива");
+int a=Convert.ToInt32(Console.Readline());
+int[] array= new int [a];
+for(int i=0; i<array.Length; i++){
+    array[i]= new Random().Next(0,100);
+}
+Console.WriteLine("["+string.Join(",",array)+"]");
